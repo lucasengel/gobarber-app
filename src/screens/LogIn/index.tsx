408e2c9e8a2d0dac29fec68d59de44377537bc72
Lucas Engel <lucasengel@gmail.com>
2020-09-27 21:47:56 -0400

    LogIn screen styling

--- src/screens/LogIn/index.tsx
+++ src/screens/LogIn/index.tsx
@@ -1,21 +1,41 @@
 import React from 'react'
-import { Image, Text } from 'react-native'
+import { View, Image, KeyboardAvoidingView, Platform } from 'react-native'
 
 import Button from '../../components/Button'
 import Input from '../../components/Input'
 
-import { Container, Title } from './styles'
+import { Container, Title, ForgotPassword, ForgotPasswordText, CreateAccountButton, CreateAccountButtonText, Icon } from './styles'
 import logoImg from '../../assets/logo.png'
+import { ScrollView } from 'react-native-gesture-handler'
 
 const LogIn: React.FC = () => {
   return (
-    <Container>
-      <Image source={logoImg}/>
-      <Title>Log In</Title>
-      <Input name="email" icon="mail" placeholder="email" />
-      <Input name="password" icon="lock" placeholder="password"/>
-      <Button onPress={() => { console.log()}}>Enter</Button>
-    </Container>
+    <>
+      <KeyboardAvoidingView
+        style={{ flex: 1 }}
+        behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+        enabled
+      >
+        <ScrollView contentContainerStyle={{ flex: 1 }} keyboardShouldPersistTaps="handled" >
+          <Container>
+            <Image source={logoImg} />
+            <View>
+              <Title>Log In</Title>
+            </View>
+            <Input name="email" icon="mail" placeholder="email" />
+            <Input name="password" icon="lock" placeholder="password" />
+            <Button onPress={() => { console.log() }}>Enter</Button>
+            <ForgotPassword>
+              <ForgotPasswordText>Forgot Password?</ForgotPasswordText>
+            </ForgotPassword>
+          </Container>
+        </ScrollView>
+      </KeyboardAvoidingView>
+      <CreateAccountButton onPress={() => { }} >
+        <Icon name="log-in" size={20} color="#ff9000" />
+        <CreateAccountButtonText>Create account</CreateAccountButtonText>
+      </CreateAccountButton>
+    </>
   )
 }
 
--- src/screens/LogIn/styles.ts
+++ src/screens/LogIn/styles.ts
@@ -1,4 +1,6 @@
 import styled from 'styled-components/native';
+import FeatherIcon from 'react-native-vector-icons/Feather'
+import { getBottomSpace } from 'react-native-iphone-x-helper';
 
 export const Container = styled.View`
   flex: 1;
@@ -12,4 +14,36 @@ export const Title = styled.Text`
   color: #f4ede8;
   font-family: 'RobotoSlab-Medium';
   margin: 64px 0 24px;
+`
+
+export const ForgotPassword = styled.TouchableOpacity`
+  margin-top: 24px;
+`
+
+export const ForgotPasswordText = styled.Text`
+  color: #f4ede8;
+  font-size: 16px;
+  font-family: 'RobotoSlab-Regular';
+`
+
+export const CreateAccountButton = styled.TouchableOpacity`
+  align-items: center;
+  background: #312e38;
+  flex-direction: row;
+  justify-content: center;
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  border-top-width: 1px;
+  border-color: #232129;
+  padding: 16px 0 ${16 + getBottomSpace() + 'px'};
+`
+export const CreateAccountButtonText = styled.Text`
+  color: #ff9000;
+  font-family: 'RobotoSlab-Regular';
+  font-size: 18px;
+`
+export const Icon = styled(FeatherIcon)`
+  margin-right: 16px;
 `
