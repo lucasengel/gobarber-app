77119cd5d93ebb88437ef2eebbd653e03130ff0c
Lucas Engel <lucasengel@gmail.com>
2020-10-28 20:36:37 -0400

    AppointmentCreated Screen

--- src/screens/AppointmentCreated/index.tsx
+++ src/screens/AppointmentCreated/index.tsx
@@ -1,9 +1,48 @@
-import React from 'react'
-import { Container } from './styles'
+import { useNavigation, useRoute } from '@react-navigation/native'
+import { format } from 'date-fns'
+import React, { useCallback, useMemo } from 'react'
+import Icon from 'react-native-vector-icons/Feather'
+import {
+  Container,
+  Title,
+  Description,
+  OkButton,
+  OkButtonText,
+} from './styles'
+
+interface RouteParams {
+  date: number;
+}
 
 const AppointmentCreated: React.FC = () => {
+  const { reset } = useNavigation()
+  const { params } = useRoute()
+
+  const routeParams = params as RouteParams;
+
+  const formattedDate = useMemo(() => {
+    return format(routeParams.date, "EEEE, LLLL do 'at' h:mma")
+  }, [routeParams.date])
+
+  const handleOkPress = useCallback(
+    () => {
+      reset({
+        routes: [{ name: 'Dashboard' }],
+        index: 0
+      })
+    },
+    [reset],
+  )
+
   return (
-    <Container></Container>
+    <Container>
+      <Icon name="check" size={80} color="#04d361" />
+      <Title>Appointment Booked</Title>
+      <Description>{formattedDate}</Description>
+      <OkButton onPress={handleOkPress}>
+        <OkButtonText>Thanks</OkButtonText>
+      </OkButton>
+    </Container>
   )
 }
 
--- src/screens/AppointmentCreated/styles.ts
+++ src/screens/AppointmentCreated/styles.ts
@@ -1,3 +1,41 @@
+import { RectButton } from "react-native-gesture-handler";
 import styled from "styled-components/native";
 
-export const Container = styled.View``
+export const Container = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+  padding: 0 24px;
+`
+
+export const Title = styled.Text`
+  color: #f4ede8;
+  font-family: 'RobotoSlab-Medium';
+  font-size: 32px;
+  margin-top: 48px;
+  text-align: center;
+`
+
+export const Description = styled.Text`
+  color: #999591;
+  font-family: 'RobotoSlab-Regular';
+  font-size: 18px;
+  margin-top: 16px;
+  text-align: center;
+`
+
+export const OkButton = styled(RectButton)`
+  align-items: center;
+  background-color: #ff9000;
+  border-radius: 10px;
+  justify-content: center;
+  margin-top: 24px;
+  padding: 12px 24px;
+`
+
+export const OkButtonText = styled.Text`
+  color: #312e38;
+  font-family: 'RobotoSlab-Medium';
+  font-size: 18px;
+  margin-left: 8px;
+`
